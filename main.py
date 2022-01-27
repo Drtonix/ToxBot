c4ae02cd59473c373acddead57ffae050b004967
kilersbot <77185723+kilersbot@users.noreply.github.com>
2022-01-27 09:17:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -321,7 +321,7 @@ async def anime(ctx):
 @bot.command()
 async def mechanic(ctx, *, link: str):
     if link != None:
-        rplay(ctx, str(link))
+        await rplay(ctx, str(link))
         await ctx.send("удачно поставлено")
     else:
         await ctx.send("вставьте ссылку")
