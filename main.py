029ecd3d84c083ee5e0870a93bd6967bd4187f4d
Drtonix <84413493+Drtonix@users.noreply.github.com>
2022-02-20 13:18:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from datetime import datetime
 from random import randrange, choice
 from pyrandmeme import *
+import pyrandmeme
 import string
 import time
 import random
