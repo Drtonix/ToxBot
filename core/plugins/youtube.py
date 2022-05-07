4f3fd4a30c310de4e7c8642a17b16295be29656b
Ampernic <44705058+Ampernic@users.noreply.github.com>
2022-05-07 22:14:04 +0300

    Тестовая версия [+] Залупление

--- core/plugins/youtube.py
+++ core/plugins/youtube.py
@@ -22,12 +22,14 @@ def yt(bot, data):
                                    'options': '-vn'}
 
             # Внутренние переменные модуля
-            self.voice_channel = None       # Хранилище канала воспроизведения
-            self.voice_client = None        # Хранилище клиента воспроизведения
-            self.is_playing = False         # Состояние клиента воспроизведения
+            self.voice_channel = None  # Хранилище канала воспроизведения
+            self.voice_client = None  # Хранилище клиента воспроизведения
 
-            self.q = []                     # Массив очереди
-            self.q_now = 0                  # Счетчик очереди
+            self.is_playing = False  # Состояние клиента воспроизведения
+            self.loop = None  # Состояние "залупления" всех
+
+            self.q = []  # Массив очереди
+            self.q_now = 0  # Счетчик очереди
 
             # Общий метод бота
             self.bot = bot
@@ -53,20 +55,21 @@ def yt(bot, data):
                                 Убедитесь, что вы ввели название правильно
                                 И что ваш трек есть в библиотеке YouTube
                                 _*(Если это какая то ошибка - попробуйте указать ссылку)*_''',
-                                f'ToxBot v{num_ver} - всегда рад помочь',
-                                'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
+                                         f'ToxBot v{num_ver} - всегда рад помочь',
+                                         'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
                         track_found = False
 
                 # Если трек найден
                 if track_found:
 
                     # Сохраняем информацию о треке который нашли
-                    url = info['formats'][4]['url']                                                     # Ссылка на поток
-                    track_name = info['title']                                                          # Название трека
-                    track_time = info['duration_string']                                                # Длительность трека
-                    track_author = ctx.message.author.name                                              # Кто запросил трек
-                    track_thumbnail = info['thumbnail']                                                 # Превью трека
-                    self.q.append([url, track_name, track_time, track_author, track_thumbnail])         # Запись данных в массив
+                    url = info['formats'][4]['url']  # Ссылка на поток
+                    track_name = info['title']  # Название трека
+                    track_time = info['duration_string']  # Длительность трека
+                    track_author = ctx.message.author.name  # Кто запросил трек
+                    track_thumbnail = info['thumbnail']  # Превью трека
+                    self.q.append(
+                        [url, track_name, track_time, track_author, track_thumbnail])  # Запись данных в массив
 
                     # Пытаемся запустить воспроизведение
                     try:
@@ -78,8 +81,8 @@ def yt(bot, data):
                                 Трек: _*{self.q[0][1]}*_
                                 Продолжительность: _*{self.q[0][2]}*_
                                 ''',
-                                f'Запросил: {self.q[0][3]}',
-                                self.q[0][4])  # Тут превью видео
+                                             f'Запросил: {self.q[0][3]}',
+                                             self.q[0][4])  # Тут превью видео
 
                             # Запускаем первый трек
                             await self.playing(url, ctx)
@@ -90,8 +93,8 @@ def yt(bot, data):
                                 Трек: _*{track_name}*_
                                 Продолжительность: _*{track_time}*_
                                 ''',
-                                f'Запросил: {track_author}',
-                                track_thumbnail)  # Тут превью видео
+                                             f'Запросил: {track_author}',
+                                             track_thumbnail)  # Тут превью видео
                     except Exception as e:
                         await ctx.send(str(e))
 
@@ -99,33 +102,48 @@ def yt(bot, data):
             try:
                 # Проверяем, остались ли еще треки в очереди
                 if self.q_now < len(self.q) - 1:
-                    self.q_now += 1                                                                     # Увеличиваем счетчик очереди
+                    if self.loop == 'all' or self.loop is None:
+                        self.q_now += 1  # Увеличиваем счетчик очереди если нет лупа на 1 трек
 
                     # Выводим информацию о следующем треке
                     await send_embed(ctx, '🎵 Сейчас играет 🎵', f'''
                                     Трек: _*{self.q[self.q_now][1]}*_
                                     Время: _*{self.q[self.q_now][2]}*_''',
-                                    f'Запросил: {self.q[self.q_now][3]}',
-                                    self.q[self.q_now][4])
+                                     f'Запросил: {self.q[self.q_now][3]}',
+                                     self.q[self.q_now][4])
 
                     # Запускаем воспроизведение
                     await self.playing(self.q[self.q_now][0], ctx)
 
-                # Если треки кончились и бот еще в войсе - отключаем бота от войса
+                # Если треки кончились и бот еще в войсе
                 else:
-                    if self.voice_client:
+                    if self.loop is not None:  # Если "залуплено" - стартуем с начала
+                        # Обнуляем счетчик
+                        self.q_now = 0
+
+                        # Выводим информацию о следующем треке
+                        await send_embed(ctx, '🎵 Сейчас играет 🎵', f'''
+                                        Трек: _*{self.q[self.q_now][1]}*_
+                                        Время: _*{self.q[self.q_now][2]}*_''',
+                                         f'Запросил: {self.q[self.q_now][3]}',
+                                         self.q[self.q_now][4])
+
+                        # Запускаем воспроизведение
+                        await self.playing(self.q[self.q_now][0], ctx)
+
+                    elif self.loop is None and self.voice_client:  # Нет - кикаем
                         await self.stop_playing(ctx)
             except:
-                pass  # Да, это необходимо чтобы бот не подсирал в чат хуй знает откуда взятые попытки воспроизведения
+                pass    # Да, это необходимо чтобы бот не подсирал в чат хуй знает откуда взятые попытки воспроизведения
 
         async def stop_playing(self, ctx):
             try:
                 # Проверяем играет ли вообще бот, если да - отключаем
                 if self.voice_client.is_connected and self.is_playing:
-                    await self.voice_client.disconnect()                                            # Отключаем бота
-                    self.is_playing = False                                                         # Снимаем флаг
-                    self.q_now = 0                                                                  # Обнуляем счетчик очереди
-                    self.q = []                                                                     # Отчищаем массив очереди
+                    await self.voice_client.disconnect()  # Отключаем бота
+                    self.is_playing = False  # Снимаем флаг
+                    self.q_now = 0  # Обнуляем счетчик очереди
+                    self.q = []  # Отчищаем массив очереди
                     await send_embed(ctx, '🔇 Воспроизведение остановлено 🔇',
                                      f'Кто-то выдернул кабель питания из розетки :<',
                                      f'Прервал: {ctx.message.author.name}',
@@ -137,14 +155,14 @@ def yt(bot, data):
             # Проверяем не подключен ли уже бот к войсу
             if self.voice_client:
                 try:
-                    self.voice_client.pause()                                                       # Ставим на паузу и лишь потом включаем
+                    self.voice_client.pause()  # Ставим на паузу и лишь потом включаем
                     if data["OS"] == 1:
                         self.voice_client.play(discord.FFmpegPCMAudio(executable=r"./ffmpeg/ffmpeg.exe", source=url,
-                                                **self.FFMPEG_OPTIONS))
+                                                                      **self.FFMPEG_OPTIONS))
                     elif data["OS"] == 0:
                         self.voice_client.play(
                             discord.FFmpegPCMAudio(executable="ffmpeg", source=url,
-                                                **self.FFMPEG_OPTIONS))
+                                                   **self.FFMPEG_OPTIONS))
 
                     self.is_playing = True
 
@@ -159,11 +177,11 @@ def yt(bot, data):
                     self.voice_client = await self.voice_channel.connect()
                     if data["OS"] == 1:
                         self.voice_client.play(discord.FFmpegPCMAudio(executable=r"./ffmpeg/ffmpeg.exe", source=url,
-                                                **self.FFMPEG_OPTIONS))
+                                                                      **self.FFMPEG_OPTIONS))
                     elif data["OS"] == 0:
                         self.voice_client.play(
                             discord.FFmpegPCMAudio(executable="ffmpeg", source=url,
-                                                **self.FFMPEG_OPTIONS))
+                                                   **self.FFMPEG_OPTIONS))
                     self.is_playing = True
 
                     while self.voice_client.is_playing():
@@ -187,18 +205,42 @@ def yt(bot, data):
                 await send_embed(ctx, '❌ Не удалось запустить воспроизведение ❌', '''
                                 Вы не находитесь в войсе
                                 Вы можете добавить трек в очередь только находясь в войс-чате''',
-                                'Зайди наконец в войс, бака w_w',
-                                'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
+                                 'Зайди наконец в войс, бака w_w',
+                                 'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
         else:
             await send_embed(ctx, '❌ Не удалось добавить трек ❌', '''
                             Вы не ввели название трека или ссылку
                             _* Партия не довольна вами *_''',
-                            f'ToxBot v{num_ver} - на страже порядка',
-                            'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
+                             f'ToxBot v{num_ver} - на страже порядка',
+                             'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
 
     @bot.command(pass_context=True)
     async def next(ctx):
         await ytx.next_track(ctx)
+
+    @bot.command(pass_context=True)
+    async def loop(ctx, type=None):
+        if ytx.loop == 'all' and type is None:
+            ytx.loop = type
+            await send_embed(ctx, '🎵 Повтор отключен 🎵', '''
+                            Текущий режим повтора: _*Выключен*_
+                            После окончания очереди бот отключится''',
+                            'Но ты всегда можешь добавить еще треков...',
+                            'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
+        elif (ytx.loop is None or ytx.loop == 'one') and type == 'all':
+            ytx.loop = type
+            await send_embed(ctx, '🎵 Повтор включен 🎵', '''
+                            Текущий режим повтора: _*Включен (Все треки)*_
+                            Повторяется вся очередь.''',
+                            'Вы можете добавлять еще треки в очередь...',
+                            'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
+        elif (ytx.loop is None or ytx.loop == 'all') and type == 'one':
+            ytx.loop = type
+            await send_embed(ctx, '🎵 Повтор отключен 🎵', '''
+                            Текущий режим повтора: _*Включен (Один трек)*_
+                            Повторяется лишь текущий трек.''',
+                            'Когда он тебе надоест - можешь отключить или сменить режим...',
+                            'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
 
     @bot.command(pass_context=True)
     async def stop(ctx):
@@ -210,6 +252,6 @@ def yt(bot, data):
                              'Но кстати ты всегда можешь что-то включить...',
                              'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png')
 
-    # @bot.command()     команда для дебага
-    # async def qnow(ctx):
-    #    await ctx.send(f'qnow: {ytx.q_now} \n len(q): {len(ytx.q)}')
+    @bot.command()
+    async def qnow(ctx):
+        await ctx.send(f'qnow: {ytx.q_now} \n len(q): {len(ytx.q)} \n loop type: {ytx.loop}')
