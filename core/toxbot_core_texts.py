e5ff064192fbe13cbb1308cfa0003ea88f7661a6
Tonix <84413493+Drtonix@users.noreply.github.com>
2022-06-08 22:00:40 +0300

    Что нового: Микрофиксы

--- core/toxbot_core_texts.py
+++ core/toxbot_core_texts.py
@@ -1,18 +1,8 @@
-num_ver = '''0.4.9.7e'''
+num_ver = '''0.4.9.7f'''
 water = "- ToxBot#1253 -"
 text_ver = '''
 Что нового:
 Микрофиксы
-
-Пофикшено:
-[+] Ошибки при наборе служб
-[+] Больше нельзя абузить пинги через:
-```++kill
-++fuck
-++eat
-++twisted
-++give```
-Пинги заменяются на текст
 '''
 
 default_thumbnail = 'https://media.discordapp.net/attachments/939136925095297055/943240401031135303/ToxDsBot.png'
